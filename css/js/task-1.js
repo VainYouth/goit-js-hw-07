d1db7b84c955adc031d5fa7dce07460a1a77c298
Yinest <80900691+VainYouth@users.noreply.github.com>
2025-11-03 00:31:55 +0300

    seven homework

---
+++ css/js/task-1.js
@@ -0,0 +1,10 @@
+const listCategories = document.querySelectorAll("#categories li.item");
+console.log(`Number of categories: ${listCategories.length}`);
+
+listCategories.forEach((item) => {
+  const categoryName = item.querySelector("h2").textContent;
+  const itemCount = item.querySelectorAll("li").length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${itemCount}`);
+});
